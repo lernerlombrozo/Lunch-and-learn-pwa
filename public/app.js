79896b59550ba6a51b0561a11473e95f3d05152b
David Lerner <d.lerner@ghostlab.ca>
2021-02-07 14:04:30 -0700

    caching

--- public/app.js
+++ public/app.js
@@ -2,6 +2,10 @@ if('serviceWorker' in navigator){
   navigator.serviceWorker
     .register('/sw.js')
     .then(()=>{
-      console.log('service worker registered!')
+      console.log('[app.js] service worker registered!');
     })
 }
+
+window.addEventListener('beforeinstallprompt',() => {
+  console.log('[app.js] before install prompt fired');
+})
--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,76 @@
-self.addEventListener('install', (event)=>{
-  console.log('[Service Worker] installing service worker ...', event);
-})
 
-self.addEventListener('activate', (event)=>{
-  console.log('[Service Worker] activating service worker ...', event);
-  return self.clientInformation.claim();
-})
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
-self.addEventListener('fetch', (event)=>{
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  precache(event);
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  clearCache(event);
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
   console.log('[Service Worker] Fetching ...', event);
-  // event.respondWith(null)
-  event.respondWith(fetch(event.request));
-})
+  cache(event);
+});
+
+
+function precache(event){
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then((cache) => {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/404.html',
+          '/app.js',
+          '/css/normalize.css',
+          '/css/skeleton.css',
+          '/images/icon-chan.png',
+          'https://fonts.googleapis.com/css?family=Raleway:400,300,600',
+        ]);
+      })
+  )
+}
+
+function cache(event){
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then((res) => {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then((cache) => {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch((err) => {
+              console.log(err)
+            });
+        }
+      })
+  );
+}
+
+function clearCache(event){
+  event.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key)=> {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+}
