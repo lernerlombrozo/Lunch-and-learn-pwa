aa84e6cd11ed7c7cf022d2742f162be004f46c1f
David Lerner <d.lerner@ghostlab.ca>
2021-02-10 09:42:10 -0700

    position in one function

--- public/characteristics/geolocation/geolocation.js
+++ public/characteristics/geolocation/geolocation.js
@@ -1,14 +1,12 @@
 const latitudeElement = document.getElementById("latitude");
 const longitudeElement = document.getElementById("longitude");
 
-const showPosition = (position) => {
-  latitudeElement.innerHTML = position.coords.latitude; 
-  longitudeElement.innerHTML = position.coords.longitude;
-}
-
 const getGeolocation = () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition((position) => {
+      latitudeElement.innerHTML = position.coords.latitude; 
+      longitudeElement.innerHTML = position.coords.longitude;
+    });
   } else { 
     const notSupportedMessage = "Geolocation is not supported by this browser.";
     latitudeElement.innerHTML = notSupportedMessage;
