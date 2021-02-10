75185f70a5160c2a64cfea88adab48481d417767
David Lerner <d.lerner@ghostlab.ca>
2021-02-10 09:49:40 -0700

    reverted geolocation

--- public/characteristics/geolocation/geolocation.js
+++ public/characteristics/geolocation/geolocation.js
@@ -1,12 +1,14 @@
 const latitudeElement = document.getElementById("latitude");
 const longitudeElement = document.getElementById("longitude");
 
+const showPosition = (position) => {
+  latitudeElement.innerHTML = position.coords.latitude; 
+  longitudeElement.innerHTML = position.coords.longitude;
+}
+
 const getGeolocation = () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      latitudeElement.innerHTML = position.coords.latitude; 
-      longitudeElement.innerHTML = position.coords.longitude;
-    });
+    navigator.geolocation.getCurrentPosition(showPosition);
   } else { 
     const notSupportedMessage = "Geolocation is not supported by this browser.";
     latitudeElement.innerHTML = notSupportedMessage;
